8f913f35541524a8fb19ce49bf3146b9e265f2c1
Corbin Page <corbin.page@gmail.com>
2016-10-16 15:21:06 -0400

    Changed prod DB config

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,7 @@
 var Sequelize = require('sequelize');
 
 if(process.env.NODE_ENV === 'production') {
-  var db = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USERNAME, process.env.POSTGRES_PASSWORD, {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+  var db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
 
     pool: {
