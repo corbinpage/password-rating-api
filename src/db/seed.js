62d86d7f9159fc2e3474f722d40f5cda9c17a3bc
Corbin Page <corbin.page@gmail.com>
2016-10-17 23:32:40 -0400

    Adding password strength

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,63 +1,21 @@
 var Sequelize = require('sequelize');
 var fs = require('fs');
-
-if(process.env.NODE_ENV === 'production') {
-  var db = new Sequelize(process.env.POSTGRES_DATABASE_URL_COPPER, {
-    dialect: 'postgres',
-
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  });  
-} else {
-  var db = new Sequelize('password-rating', 'corbinpage', 'password', {
-    host: 'localhost',
-    dialect: 'postgres', // 'sqlite'
-
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  });
-}
-
-var Password = db.define('password', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  text: {
-    type: Sequelize.STRING
-  },
-  rank: {
-    type: Sequelize.INTEGER
-  },
-  strength: {
-    type: Sequelize.STRING
-  },
-  score: {
-    type: Sequelize.INTEGER
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
+var db = require('/db');
+var Password = require('../models/password');
 
 Password.sync({force: true}).then( o => {
 
   var seedPasswords = fs.readFileSync('src/lib/10_million_password_list_top_10000.txt').toString().split("\n");
   var rank = 1;
-  seedPasswords.forEach(p => {
-    Password.create({
+  var passwords = seedPasswords.map(p => {
+    var newPassword = Password.build({
       text: p,
-      rank: rank,
-      strength: "Weak",
-      score: 1
-    })
+      rank: rank
+    });
+    
+    newPassword.testStrength();
     rank++;
+    return newPassword
   })
   
 });
--- src/models/password.js
+++ src/models/password.js
@@ -1,5 +1,10 @@
 import db from '../db/db';
 import Sequelize from 'sequelize';
+import taiPasswordStrength from 'tai-password-strength';
+
+if (betterResults.strengthCode.indexOf('WEAK') >= 0) {
+  throw new Error("Your password is too weak");
+}
 
 var Password = db.define('password', {
   id: {
@@ -18,11 +23,53 @@ var Password = db.define('password', {
   },
   score: {
     type: Sequelize.INTEGER
+  },
+  textLength: {
+    type: Sequelize.INTEGER
+  },
+  containsLowercase: {
+    type: Sequelize.BOOLEAN
+  },
+  containsUppercase: {
+    type: Sequelize.BOOLEAN
+  },
+  containsNumbers: {
+    type: Sequelize.BOOLEAN
+  },
+  containsSymbols: {
+    type: Sequelize.BOOLEAN
+  },
+  containsOther: {
+    type: Sequelize.BOOLEAN
   }
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
 Password.sync();
+
+Password.prototype.rankDescription = function() {
+  if(this.get('rank')) {
+    return  'Your password is in the top '&this.get('rank')&' most common passwords';
+  } else {
+    return 'Your password is not in the top 10,000 most common passwords.';
+  }
+};
+
+Password.prototype.testStrength = function() {
+  var strengthTester = new taiPasswordStrength.PasswordStrength();
+  var results = strengthTester.check(this.text);
+
+  this.rank = this.rank || 0;
+  this.score = charsetSize;
+  this.textLength = results.passwordLength;
+  this.strength = results.strengthCode;
+  this.containsLowercase = results.charets.lower;
+  this.containsUppercase = results.charets.upper;
+  this.containsNumbers = results.charets.number;
+  this.containsSymbols = results.charets.symbol;
+  this.containsOther = results.charets.other;
+
+};
 
 export default Password;
