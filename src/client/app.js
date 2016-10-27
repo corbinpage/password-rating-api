ccdf1355959806df5d5046f3a3db1a181448e2c8
Corbin Page <corbin.page@gmail.com>
2016-10-26 23:37:03 -0400

    Finished styling the main page

--- src/client/app.js
+++ src/client/app.js
@@ -1,21 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: '',
-    rating: '<response>'
+    passwordText: '',
+    rating: '<response>',
+    alert: ''
   },
   methods: {
-    reverseMessage: function () {
-      this.message = this.message.split('').reverse().join('')
-    },
     checkRating: function () {
-      console.log("asdf");
       let thisVue = this;
       $.get({
-        url: 'https://password-rating.herokuapp.com/api/v1/passwords/' + encodeURIComponent(this.message),
+        url: 'https://password-rating.herokuapp.com/api/v1/passwords/' + encodeURIComponent(this.passwordText),
       }).done(function( data ) {
         thisVue.rating = data;
-      });
+      }).fail(function() {
+        thisVue.alert = 'Uh oh, something went wrong. :-(';
+          thisVue.rating = '';
+        });
     }
   }
 })
